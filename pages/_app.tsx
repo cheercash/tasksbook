16d63e7a69af0650f653407eb4b50b5f49b829b4
cheercash <d.paiusov@yandex.ru>
2023-12-10 19:02:52 +0500

    fix(tailwind): create styles & add import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { appWithTranslation } from "next-i18next";
 import { AppProps } from "next/app";
 
+import "root/index.css";
+
 const App = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 );
