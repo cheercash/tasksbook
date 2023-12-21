73275c75b9b16d7b67bfe3e1d5e002bbe6142cdd
cheercash <d.paiusov@yandex.ru>
2023-12-21 18:08:47 +0500

    fix(home): remove unnecessary code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,5 @@
-import {
-  CocaColaIcon,
-  MenuItem,
-  SuccessIcon,
-  WarningIcon,
-} from "root/shared/ui/atoms";
-import { Select } from "root/shared/ui/molecules/select";
-
 const Index = () => {
-  return (
-    <>
-      <Select
-        title="Select something"
-        icon={<CocaColaIcon />}
-      >
-        <MenuItem
-          value="1"
-          icon={<WarningIcon />}
-        >
-          Item 1
-        </MenuItem>
-        <MenuItem value="2">Item 2</MenuItem>
-        <MenuItem
-          value="3"
-          icon={<SuccessIcon />}
-        >
-          Item 3
-        </MenuItem>
-        <MenuItem
-          value="4"
-          disabled
-        >
-          Item 4
-        </MenuItem>
-      </Select>
-    </>
-  );
+  return <>home</>;
 };
 
 export default Index;
